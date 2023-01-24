01d09532e5dff98d224018ff8d99617242ad7fee
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-01-24 13:31:44 -0300

    Primeiro programa

--- Modulo1-CSharpBasico/CSharpBasico/Program.cs
+++ Modulo1-CSharpBasico/CSharpBasico/Program.cs
@@ -1,12 +1,17 @@
-﻿static void Main(string[] args)
+﻿public class Program
 {
-    string origem = "Porto Alegre";
-    string destino = "São Paulo";
-    int duracao = 2;
+    //Todo programa possui o método main.
+    static void Main(string[] args)
+    {
+        string origem = "Porto Alegre";
+        string destino = "São Paulo";
+        int duracao = 2;
 
-    Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
+        Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
 
-    //Interpolação de strings
-    //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
-    Console.ReadLine();
+        //Interpolação de strings
+        //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
+        Console.ReadLine();
+    }
 }
+
