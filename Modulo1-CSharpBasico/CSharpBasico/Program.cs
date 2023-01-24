adadb5fa2eedb896cacab8e5a550d5adad9b59e1
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-01-24 14:00:03 -0300

    Explicando métodos

--- Modulo1-CSharpBasico/CSharpBasico/Program.cs
+++ Modulo1-CSharpBasico/CSharpBasico/Program.cs
@@ -1,12 +1,16 @@
-﻿static void Main(string[] args)
+﻿public class Program
 {
-    string origem = "Porto Alegre";
-    string destino = "São Paulo";
-    int duracao = 2;
+    static void Main(string[] args)
+    {
+        string origem = "Porto Alegre";
+        string destino = "São Paulo";
+        int duracao = 2;
 
-    Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
+        Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
 
-    //Interpolação de strings
-    //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
-    Console.ReadLine();
+        //Interpolação de strings
+        //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
+        Console.ReadLine();
+    }
+
 }
