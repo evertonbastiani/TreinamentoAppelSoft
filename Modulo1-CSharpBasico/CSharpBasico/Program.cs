2d1b71c6d87335e0beaa045dc11aed8739091a2c
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-01-24 14:50:13 -0300

    Explicando métodos

--- Modulo1-CSharpBasico/CSharpBasico/Program.cs
+++ Modulo1-CSharpBasico/CSharpBasico/Program.cs
@@ -2,15 +2,31 @@
 {
     static void Main(string[] args)
     {
-        string origem = "Porto Alegre";
-        string destino = "São Paulo";
-        int duracao = 2;
+        ExibirRota("Porto Alegre", "São Paulo", 2);
+        ExibirRota("Porto Alegre", "São Paulo", 2, 150);
 
-        Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
-
-        //Interpolação de strings
-        //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
         Console.ReadLine();
+    }
+
+    //Método vazio, sem retorno.
+    static void ExibirRota(string origem, string destino, int duracao)
+    {
+        Console.WriteLine($"O caminhão vai partir de {origem} para {destino} com duração de {duracao} dias.");
+    }
+
+    //Exemplo de Sobrecarga (Overload). Vermos isto melhor quando iniciarmos o Módulo2: POO
+    static void ExibirRota(string origem, string destino, int duracao, decimal distancia)
+    {
+        var valorFrete = calcularValorFrete(150);
+        Console.WriteLine($"O caminhão vai partir de {origem} para {destino} com duração de {duracao} dias. Veja o valor do frete abaixo:");
+        Console.WriteLine($"O valor do frete será {valorFrete} reais.");
+    }
+
+    //Método com retorno.
+    static decimal calcularValorFrete(decimal distancia)
+    {
+        decimal valorKm = 120;
+        return (distancia * valorKm);
     }
 
 }
