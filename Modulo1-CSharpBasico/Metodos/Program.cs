a5537bb0537822cfee30a80f0ee58b098dc9f9de
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-01-24 14:34:36 -0300

    Tipo de dado Enumerator

--- Modulo1-CSharpBasico/Metodos/Program.cs
+++ Modulo1-CSharpBasico/Metodos/Program.cs
@@ -9,24 +9,48 @@
         Console.ReadLine();
     }
 
-    //Método vazio, sem retorno.
     static void ExibirRota(string origem, string destino, int duracao)
     {
         Console.WriteLine($"O caminhão vai partir de {origem} para {destino} com duração de {duracao} dias.");
     }
 
-    //Exemplo de Sobrecarga (Overload). Vermos isto melhor quando iniciarmos o Módulo2: POO
+    //Exemplo de Sobrecarga (Overload)
     static void ExibirRota(string origem, string destino, int duracao, decimal distancia)
     {
-        var valorFrete = calcularValorFrete(150);
+        var valorFrete = calcularValorFrete(150, TipoProduto.NaoPereciveis);
         Console.WriteLine($"O caminhão vai partir de {origem} para {destino} com duração de {duracao} dias. Veja o valor do frete abaixo:");
         Console.WriteLine($"O valor do frete será {valorFrete} reais.");
     }
 
-    //Método com retorno.
     static decimal calcularValorFrete(decimal distancia)
     {
         decimal valorKm = 120;
         return (distancia * valorKm);
     }
+
+    static decimal calcularValorFrete(decimal distancia, TipoProduto tipoProduto)
+    {
+        decimal valorKm = 0;
+        if (tipoProduto == TipoProduto.Combustiveis)
+        {
+            valorKm = 500;
+        }
+        if (tipoProduto == TipoProduto.Pereciveis)
+        {
+            valorKm = 100;
+        }
+        if (tipoProduto == TipoProduto.NaoPereciveis)
+        {
+            valorKm = 55.9M;
+        }
+
+        return (distancia * valorKm);
+    }
+}
+
+public enum TipoProduto
+{
+    Combustiveis = 1,
+    Pereciveis = 2,
+    NaoPereciveis = 3
 }
