580a1d30d210a2a54040718d0bc3ae7bd0e13ce6
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-07 14:40:41 -0300

    CSharp Básico

--- Modulo1-CSharpBasico/01-CSharpBasico/Program.cs
+++ Modulo1-CSharpBasico/01-CSharpBasico/Program.cs
@@ -5,9 +5,9 @@
     {
         string origem = "Porto Alegre";
         string destino = "São Paulo";
-        int duracao = 2;
+        int distancia = 1130;
 
-        Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
+        Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + ". A distância até o destino é de " + distancia + " kilômetros.");
 
         //Interpolação de strings
         //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
