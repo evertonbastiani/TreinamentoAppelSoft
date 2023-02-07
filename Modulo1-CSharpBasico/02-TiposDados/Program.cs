221fa15f0b411e9ce6f98e90993c0d8860947f4d
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-07 15:06:17 -0300

    Tipos de dados

---
+++ Modulo1-CSharpBasico/02-TiposDados/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TiposDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCurso = "Curso .NET Módulo 1";
+            int duracaoCurso = 2; //semanas
+            DateTime dataInicial = new DateTime(2023, 02, 13);
+            DateTime dataFinal = new DateTime(2023, 02, 23);
+
+            Console.WriteLine("Dados do Curso \r\n");
+            Console.WriteLine($"Nome do curso: {nomeCurso}");
+            Console.WriteLine($"Duração do curso: {duracaoCurso} semanas");
+            Console.WriteLine($"Data inicial: {dataInicial}");
+            Console.WriteLine($"Data final: {dataFinal}");
+            
+            Console.ReadLine();
+        }
+    }
+}
