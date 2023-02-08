eeba63aa0386297c17045bbeff7cfafc1e4f0a54
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-08 11:36:41 -0300

    Constantes

--- Modulo1-CSharpBasico/01-PrimeiroPrograma/Program.cs
+++ Modulo1-CSharpBasico/01-PrimeiroPrograma/Program.cs
@@ -3,6 +3,7 @@
     //Todo programa possui o método main.
     static void Main(string[] args)
     {
+        //Variáveis locais
         string origem = "Porto Alegre";
         string destino = "São Paulo";
         int distancia = 1130;
@@ -10,7 +11,7 @@
         Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + ". A distância até o destino é de " + distancia + " kilômetros.");
 
         //Interpolação de strings
-        //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
+        Console.WriteLine($"O caminhão vai partir de {origem} para {destino}.  A distância até o destino é de {distancia} kilômetros.");
         Console.ReadLine();
     }
 }
--- Modulo1-CSharpBasico/02-TiposDados/Program.cs
+++ Modulo1-CSharpBasico/02-TiposDados/Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
+        //Variáveis globais
+        const string nomePrograma = "Estudo CSharp";
+        static string nomeCurso = "Curso .NET Módulo 1";
         static void Main(string[] args)
         {
-            string nomeCurso = "Curso .NET Módulo 1";
+            //Variáveis locais
+           
             int duracaoCurso = 2; //semanas
             DateTime dataInicial = new DateTime(2023, 02, 13);
             DateTime dataFinal = new DateTime(2023, 02, 23);
@@ -14,6 +18,13 @@
             Console.WriteLine($"Duração do curso: {duracaoCurso} semanas");
             Console.WriteLine($"Data inicial: {dataInicial}");
             Console.WriteLine($"Data final: {dataFinal}");
+
+            //Constantes não podem ser modificadas. 
+            //nomePrograma = "Teste";
+            Console.WriteLine($"O nome do programa é {nomePrograma}. Não conseguimos modificar uma constante.");
+
+            nomeCurso = "CSharp modificado";
+            Console.WriteLine($"Nome do curso: {nomeCurso}");
 
             Console.ReadLine();
         }
