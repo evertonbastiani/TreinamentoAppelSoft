d121314fda83ea9e6f9b6b71a99c32b0ace95153
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-08 09:42:48 -0300

    Master

---
+++ Modulo1-CSharpBasico/CSharpBasico/Program_BASE_1568.cs
@@ -0,0 +1,12 @@
+﻿static void Main(string[] args)
+{
+    string origem = "Porto Alegre";
+    string destino = "São Paulo";
+    int duracao = 2;
+
+    Console.WriteLine("O caminhão vai partir de " + origem + " para " + destino + " com duração de " + duracao + " dias.");
+
+    //Interpolação de strings
+    //Console.WriteLine($"O caminhão vai partir de {Origem} para {Destino} com duração de {duracao} dias.");
+    Console.ReadLine();
+}
