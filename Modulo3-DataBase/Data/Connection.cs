c5c4faa6b92922a30aea48c02db5cd3f6752ae43
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-09 09:07:07 -0300

    Modulo 3 - Database

---
+++ Modulo3-DataBase/Data/Connection.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Curso.Data
+{
+    public class Connection
+    {
+        private readonly string _connectionString;
+
+        public Connection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(_connectionString);
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw;
+            }
+            return conn;
+        }
+    }
+}
