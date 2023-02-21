9c118553ece8987f56ca5955400fa6de87d8d7b6
evertonbastiani@gmail.com <evertonbastiani@gmail.com>
2023-02-21 09:59:08 -0300

    Removidos usings desnecessários

--- Modulo3-DataBase/Data/Repository/VeiculoRepository.cs
+++ Modulo3-DataBase/Data/Repository/VeiculoRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Curso.Domain.Entities;
 using Curso.Repository.Interfaces;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Curso.Repository.Repository
 {
